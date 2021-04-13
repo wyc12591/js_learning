7a57f4448a765feddf87e5e0772c288796409a91
Duoduo <wyc12591@163.com>
2021-04-13 09:38:43 +0800

    Stage all

---
+++ test01.js
@@ -0,0 +1,12 @@
+/*
+ * @Description: 
+ * @Author: 多多
+ * @Date: 2021-04-12 09:38:27
+ * @LastEditTime: 2021-04-12 09:44:22
+ * @LastEditors: 多多
+ */
+
+for (let i = 0; i < 5; i++) {
+
+}
+console.log(i);
